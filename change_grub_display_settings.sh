abb8feb23babdae715755f012bf6a7df8631ceda
ochalkley-advai <oliver.chalkley@advai.co.uk>
2022-10-02 19:47:02 +0100

    not sure what's changes here just saw that it's not up to date

--- change_grub_display_settings.sh
+++ change_grub_display_settings.sh
@@ -4,15 +4,15 @@
 laptop_resolution=3840x2160
 work_screen_resolution=2560x1440
 
-read -p "Please select one of the following screen resolutions:\n
+read -p "Please select one of the following screen resolutions:
 	[1]: Laptop resoltion, ${laptop_resolution}
 	[2]: Work screen resolution, ${work_screen_resolution}"  -n 1 -r
 echo    # (optional) move to a new line
-if [[ ! $REPLY = ^[1]$ ]]
+if [  $REPLY -eq 1 ]
 then 
 	echo "Changing work resolution to laptop resolution."
 	sudo sed -i "s/${work_screen_resolution}/${laptop_resolution}/g" /etc/default/grub
-elif [[ ! $REPLY = ^[2]$ ]]
+elif [  $REPLY -eq 2 ]
 then
 	echo "Changing laptop resolution to work resolution."
 	sudo sed -i "s/${laptop_resolution}/${work_screen_resolution}/g" /etc/default/grub
@@ -23,4 +23,4 @@ fi
 
 sudo update-grub 
 
-#sudo reboot
+sudo reboot
