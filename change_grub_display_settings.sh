49eb900df2c705836bc5309d597fba2523c365d7
ochalkley-advai <oliver.chalkley@advai.co.uk>
2022-11-13 19:49:47 +0000

    added home screen resolution

--- change_grub_display_settings.sh
+++ change_grub_display_settings.sh
@@ -2,20 +2,44 @@
 
 # env variables
 laptop_resolution=3840x2160
+home_screen_resolution=1920x1080
 work_screen_resolution=2560x1440
 
-read -p "Please select one of the following screen resolutions:
-	[1]: Laptop resoltion, ${laptop_resolution}
-	[2]: Work screen resolution, ${work_screen_resolution}"  -n 1 -r
+read -p "Please select the existing screen resolution:
+	[1]: Laptop resolution, ${laptop_resolution}
+	[2]: Home screen resolution, ${home_screen_resolution}
+	[3]: Work screen resolution, ${work_screen_resolution}"  -n 1 -r
 echo    # (optional) move to a new line
 if [  $REPLY -eq 1 ]
 then 
-	echo "Changing work resolution to laptop resolution."
-	sudo sed -i "s/${work_screen_resolution}/${laptop_resolution}/g" /etc/default/grub
+	current_screen_resolution=${laptop_resolution}
 elif [  $REPLY -eq 2 ]
 then
-	echo "Changing laptop resolution to work resolution."
-	sudo sed -i "s/${laptop_resolution}/${work_screen_resolution}/g" /etc/default/grub
+	current_screen_resolution=${home_screen_resolution}
+elif [  $REPLY -eq 3 ]
+then
+	current_screen_resolution=${work_screen_resolution}
+else
+    echo "Incorrect selection, exiting script."
+    exit 0
+fi
+read -p "Please select one of the following screen resolutions to change to:
+	[1]: Laptop resoltion, ${laptop_resolution}
+	[2]: Laptop resoltion, ${home_screen_resolution}
+	[3]: Work screen resolution, ${work_screen_resolution}"  -n 1 -r
+echo    # (optional) move to a new line
+if [  $REPLY -eq 1 ]
+then 
+	echo "Changing resolution to laptop resolution."
+	sudo sed -i "s/${current_screen_resolution}/${laptop_resolution}/g" /etc/default/grub
+elif [  $REPLY -eq 2 ]
+then
+	echo "Changing resolution to home screen resolution."
+	sudo sed -i "s/${current_screen_resolution}/${home_screen_resolution}/g" /etc/default/grub
+elif [  $REPLY -eq 3 ]
+then
+	echo "Changing resolution to work resolution."
+	sudo sed -i "s/${current_screen_resolution}/${work_screen_resolution}/g" /etc/default/grub
 else
     echo "Incorrect selection, exiting script."
     exit 0
