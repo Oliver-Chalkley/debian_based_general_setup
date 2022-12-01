1891f6816e35d526c68da85384539d098d303e58
ochalkley-advai <oliver.chalkley@advai.co.uk>
2022-12-01 08:51:51 +0000

    changed typo in grub and added pluggins to vimrc

--- change_grub_display_settings.sh
+++ change_grub_display_settings.sh
@@ -5,7 +5,7 @@ laptop_resolution=3840x2160
 home_screen_resolution=1920x1080
 work_screen_resolution=2560x1440
 
-read -p "Please select the existing screen resolution:
+read -p "Please select the current screen resolution:
 	[1]: Laptop resolution, ${laptop_resolution}
 	[2]: Home screen resolution, ${home_screen_resolution}
 	[3]: Work screen resolution, ${work_screen_resolution}"  -n 1 -r
@@ -24,8 +24,8 @@ else
     exit 0
 fi
 read -p "Please select one of the following screen resolutions to change to:
-	[1]: Laptop resoltion, ${laptop_resolution}
-	[2]: Laptop resoltion, ${home_screen_resolution}
+	[1]: Laptop resolution, ${laptop_resolution}
+	[2]: Home Screen resolution, ${home_screen_resolution}
 	[3]: Work screen resolution, ${work_screen_resolution}"  -n 1 -r
 echo    # (optional) move to a new line
 if [  $REPLY -eq 1 ]
