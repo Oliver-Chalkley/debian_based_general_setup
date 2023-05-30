626c97b6c6d180dda7026adcfaf47c38b8e6640b
Oliver Chalkley <oliver.chalkley83@gmail.com>
2023-05-30 11:10:13 +0100

    updated vim9 install

--- linux_startup.sh
+++ linux_startup.sh
@@ -3,32 +3,30 @@
 # This script installs everything neccessary for a basic coding enviornment as
 # I prefer it
 # - GNU Screen
-# - i3 Tiling manager
+# - Vim9
+# - Vim9 Plugins
 # - Setup my person windows management shortcuts (vim-esque)
 # - VIM shortcuts in terminal and readline() apps
-# - Full version of VIM
-#      - Pathogen
-#      - vim-airline
-#      - vim-rainbo
-#      - ALE
-#      - gruvbox
-#      - kite
-# - Anaconda
+# - Anaconda (NEED TO CHANGE THIS TO MINICONDA)
 
 # update and upgrade
 sudo apt update -y
 sudo apt upgrade -y
 
+# install vim9
+./install_vim.sh
+
+# install vim plugins
+./install_vim_plugins.sh
+
 # GNU Screen
 sudo apt-get install screen -y
-
-# i3 Tiling manager
-sudo apt install i3-wm i3status suckless-tools i3lock
+sudo apt install feh -y
 
 # dconf tools not install by default
 sudo add-apt-repository universe
 sudo apt update
-sudo apt install dconf-cli dconf-editor
+sudo apt install dconf-cli dconf-editor -y
 
 # setup my windows management shortcuts
 dconf load /org/gnome/desktop/wm/keybindings/ < dconf_shortcuts_backup
@@ -53,55 +51,40 @@ ln dotfiles/profile ~/.profile
 rm ~/.ssh/config
 ln dotfiles/ssh_config ~/.ssh/config
 
-# overwrite ~/.i3/config
-rm ~/.i3/config
-ln dotfiles/i3_config ~/.i3/config
-
-# VIM
-# Uninstall mini version and install fully featured version
-sudo apt remove --assume-yes vim-tiny # DO WE NEED SUDO IN AWS INSTANCE?????
-# tidy up
-sudo apt autoremove -y
-sudo apt update -y
-# install full version of vim
-sudo apt install --assume-yes vim=8.1.1719 # we want a minimum version here so upgrade after
-sudo apt update -y
-sudo apt upgrade -y
-
 # install NodeJS so that autocomplete works
-curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
+#curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+#sudo apt-get install -y nodejs
 
 # create .vimrc
 ln dotfiles/vimrc ~/.vimrc
 
 # VIM PLUGINS
 # PATHOGEN
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-# vim-airline
-git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-
-# vim-rainbow
-git clone https://github.com/frazrepo/vim-rainbow ~/.vim/bundle/vim-rainbow
-
-# vim-ale
-git clone https://github.com/dense-analysis/ale.git ~/.vim/bundle/ale
-
-# gruvbox
-git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
-
-# Kite
-# goto https://github.com/kiteco/vim-plugin
-
-# Conquer of Completion (coc) for autocomplete
-cd ~/.vim/bundle
-git clone https://github.com/neoclide/coc.nvim.git
-
-# vimspector for debugging
-cd ~/.vim/bundle
-git clone https://github.com/puremourning/vimspector
+#mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+#curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#
+## vim-airline
+#git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+#
+## vim-rainbow
+#git clone https://github.com/frazrepo/vim-rainbow ~/.vim/bundle/vim-rainbow
+#
+## vim-ale
+#git clone https://github.com/dense-analysis/ale.git ~/.vim/bundle/ale
+#
+## gruvbox
+#git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
+#
+## Kite
+## goto https://github.com/kiteco/vim-plugin
+#
+## Conquer of Completion (coc) for autocomplete
+#cd ~/.vim/bundle
+#git clone https://github.com/neoclide/coc.nvim.git
+#
+## vimspector for debugging
+#cd ~/.vim/bundle
+#git clone https://github.com/puremourning/vimspector
 
 # ANNACONDA
 wget -P /tmp https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh && bash /tmp/Anaconda3-2021.05-Linux-x86_64.sh -b 
@@ -111,3 +94,5 @@ source ~/.bashrc
 conda update --all
 
 conda update -n base -c defaults conda
+
+echo "Please restart your terminal to complete the installation"
