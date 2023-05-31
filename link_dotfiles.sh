0295276024b213b3574a8edabd8ce83cfd586307
Oliver Chalkley <oliver.chalkley83@gmail.com>
2023-05-31 10:01:01 +0100

    seperated dotfile linking from startup script

---
+++ link_dotfiles.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This script creates links from the home directory to any desired dotfiles in ~/dotfiles
+#
+# overwrite .bashrc
+rm ~/.bashrc
+ln dotfiles/bashrc ~/.bashrc
+
+# overwrite .bash_aliases
+rm ~/.bash_aliases
+ln dotfiles/bash_aliases ~/.bash_aliases
+
+# overwrite .inputrc
+rm ~/.inputrc
+ln dotfiles/inputrc ~/.inputrc
+
+# overwrite .profile
+rm ~/.profile
+ln dotfiles/profile ~/.profile
+
+# overwrite ~/.ssh/config
+rm ~/.ssh/config
+ln dotfiles/ssh_config ~/.ssh/config
+
+# create .vimrc
+ln dotfiles/vimrc ~/.vimrc
