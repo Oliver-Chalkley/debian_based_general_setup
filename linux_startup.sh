23caa4a5e4ccaae471ce66a3ccf72a3844367fcb
Oliver Chalkley <oliver.chalkley83@gmail.com>
2023-06-21 15:33:35 +0100

    added infinite bash hist

--- linux_startup.sh
+++ linux_startup.sh
@@ -42,6 +42,9 @@ sudo apt install dconf-cli dconf-editor -y
 # setup my windows management shortcuts
 dconf load /org/gnome/desktop/wm/keybindings/ < dconf_shortcuts_backup
 
+# update .bashrc
+sed -i "/^HISTSIZE=/c\HISTSIZE=-1" ~/.bashrc
+sed -i "/^HISTFILESIZE=/c\HISTFILESIZE=-1" ~/.bashrc
 # link dotfiles
 ./link_dotfiles.sh
 
