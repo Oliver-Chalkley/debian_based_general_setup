cc8c69e3ea44385ec68e1e5f9291b1a235f77783
Oliver Chalkley <oliver.chalkley@advai.co.uk>
2024-05-09 13:01:57 +0100

    fixed conda initialisation bug

--- install_anaconda.sh
+++ install_anaconda.sh
@@ -14,4 +14,6 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod 700 Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh -b
 
+echo "export PATH=~/miniconda3/bin:$PATH" >> ~/.bashrc
+
 echo "Please restart your terminal to complete the installation"
